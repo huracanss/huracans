c354c9d4bd4fb4cc8d8e2c4b1e8f0e63e32a523f
huracanss <94643055+huracanss@users.noreply.github.com>
2023-07-20 02:54:18 +0330

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xf7008a04c41e1837d4a894f17ad20bdd48334ae0";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
